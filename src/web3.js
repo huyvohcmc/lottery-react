d23b3dee17e1b8680a8231c8f38dba4822bf62f7
Huy Vo <huyvohcmc@gmail.com>
2018-07-27 11:16:44 +0700

    Fix bug for commit #9974177

--- src/web3.js
+++ src/web3.js
@@ -4,10 +4,11 @@ let web3;
 
 if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
   // We are in the browser and Metamask is running
-  const web3 = new Web3(window.web3.currentProvider);
+  web3 = new Web3(window.web3.currentProvider);
 } else {
   // We are on the server or the user don't use Metamask
   const provider = new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/59c05e5eb5a444ed8fcae6ecc34f24ff');
+  web3 = new Web3(provider);
 }
 
 export default web3;
